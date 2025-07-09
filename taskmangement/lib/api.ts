c76453624a46cf36939c8625955e15f5214b59f3
unknown <krushnachavan29@gmail.com>
2025-07-09 23:22:33 +0530

    changes updated

--- taskmangement/lib/api.ts
+++ taskmangement/lib/api.ts
@@ -11,7 +11,7 @@ interface ApiRequestOptions {
 }
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:7000", // Adjust the base URL as needed
+  baseURL: "https://liberty-backend-qolq.onrender.com",
   headers: {
     "Content-Type": "application/json",
   },
